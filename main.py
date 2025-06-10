1294713e5e097a1eb5f2ba08820c04b1298a0dfb
salip98 <cigdemb.akyol@gmail.com>
2025-06-10 22:24:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+internet_sozluk={
+    'LOL':'Gülmek',
+    'AFK':'Bilgisardan uzak',
+    'RIP':'Allah rahmet eylesin'
+}
+while True:
+    word=input('Anlamadığınız kelimeyi yazınız. Hepsini büyük harfle yazınız')
+    if word in internet_sozluk.keys():
+        print(internet_sozluk[word])
+
+    else:
+        print('Yazdığınız kelime sözlükte yok.')
